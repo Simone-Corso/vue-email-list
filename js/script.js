297246a8b6e0acc93e10fa40bf02766dc62f0d03
Simone-Corso <scorso26@gmail.com>
2023-12-12 15:44:36 +0100

    mi dice vuto e nessun errore,provo ad aggiungere una cosole log per vedere

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
-import { createApp } from 'vue'
+
+
+const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-      //inseriamo un array per random di 10 email
+    data() {
+        return {
 
-      emails: []
-    }
-  },
-  methods: {
-    generateEmails() {
-        const apiLink = 'https://flynn.boolean.careers/exercises/api/random/mail';
+            // inseriamo la variabile per array
+            emails: []
+        };
+    },
+    methods: {
+        generateEmails() {
+            const apiLink = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-        for (let i = 0; i < 10; i++){
-            axios.get(apiLink)
-            .then(response = {
-                this.emails.push(response.data);
-            })
+            for (let i = 0; i < 10; i++) {
+                axios.get(apiLink)
+                    .then(response => {
+                        this.emails.push(response.data);
+                    });
+            }
         }
-
     }
-  }
-}).mount('#app')
+}).mount('#app');
